e9bd66ac9d93beb6f885c36fcca7a0796f23b170
Edivilhian-Hipolito <97316805+Edivilhian-H@users.noreply.github.com>
2025-06-28 19:51:51 -0300

    adicionado script

---
+++ script.js
@@ -0,0 +1,6 @@
+let tarefa = document.querySelector("#tarefa")
+let addTarefa = document.querySelector(".addTarefa")
+let deleteTarefa = document.querySelector(".delete")
+
+
+console.log(deleteTarefa)
