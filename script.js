332b32d3f1c579b27829c32d2b6823e7b9ea79d9
Edivilhian-Hipolito <97316805+Edivilhian-H@users.noreply.github.com>
2025-06-30 16:18:13 -0300

    desenvolvendo a logica

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let tarefa = document.querySelector("#tarefa")
-let addTarefa = document.querySelector(".addTarefa")
-let deleteTarefa = document.querySelector(".delete")
+let addTarefa = document.querySelector(".addTarefa");
+let deleteTarefa = document.querySelector(".delete");
+
+addTarefa.addEventListener("click", () => {
+  const tarefa = document.querySelector(".addTarefa").value;
+  console.log(tarefa); // até aqui tudo certo!
+
+  const lista = document.querySelector(".lista");
+
+  const novaTarefa = document.createElement("li");
+  novaTarefa.textContent = tarefa;
+
+  lista.appendChild(novaTarefa);
+});
 
 
-console.log(deleteTarefa)
+novaTarefa.addEventListener("click", () => {
+  novaTarefa.classList.toggle("feito"); // você cria a classe 'feito' no CSS
+});
